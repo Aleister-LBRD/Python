a380bf026eb508d3b3bed54711420aaa59167883
Alexandre Lombarde <43822315+Aleister-LBRD@users.noreply.github.com>
2022-08-30 16:07:24 -0300

    update

--- Sketch.py
+++ Sketch.py
@@ -1,12 +1,4 @@
-import numpy as np
-from matplotlib import pyplot as plt
-x = np.linspace(-2,2,100)
-y1 = np.cos(np.pi * x)
-y2 = np.sin(np.pi * x)
-plt.plot(x, y1, 'go')
-plt.plot(x, y2, 'r-')
-plt.xlabel('xlabel')
-plt.ylabel('ylabel')
-plt.grid(alpha=0.4)
-plt.title('sin-cos')
-plt.show()
+import math
+
+
+dir math
